d78818c0ea4d86fbaae160c5bae3c67af646f434
Remulo Costa <remulo.costa@vulcanet.com.br>
2022-06-22 04:16:09 -0300

    Update app component to allow use apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
 /**
  * IMPORTS
  */
+import {gql} from '@apollo/client';
+import {useQuery} from '@apollo/client';
 import React from 'react';
+
+
+/**
+ * CONSTANTS AND DEFINITIONS
+ */
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`;
+
+
+/**
+ * TYPES
+ */
+interface ILesson {
+    id: string;
+    title: string;
+}
 
 
 /**
@@ -15,8 +39,17 @@ import React from 'react';
  */
 function App (): JSX.Element
 {
+  // get lessons data
+  const {data} = useQuery<{lessons: ILesson[]}>(GET_LESSONS_QUERY);
+
+  // return app component
   return (
-    <h1>Hello word!!</h1>
+    <ul>
+      {data?.lessons.map(lesson =>
+      {
+        return <li key={lesson.id}>{lesson.title}</li>;
+      })}
+    </ul>
   );
 }
 
