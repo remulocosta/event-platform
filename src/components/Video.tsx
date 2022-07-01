62b1abed0f8e608e1a96ff7405a440d970e46598
Remulo Costa <remulo.costa@vulcanet.com.br>
2022-06-30 22:16:22 -0300

    Create Video component

---
+++ src/components/Video.tsx
@@ -0,0 +1,32 @@
+/**
+ * IMPORTS
+ */
+import React from 'react';
+
+
+/**
+ * CODE
+ */
+
+/**
+ * I render the video component.
+ *
+ * :returns: video component
+ */
+function Video (): JSX.Element
+{
+  // return component
+  return (
+    <div className='flex-1'>
+      video component
+    </div>
+  );
+}
+
+
+/**
+ * EXPORTS
+ */
+export {
+  Video
+};
